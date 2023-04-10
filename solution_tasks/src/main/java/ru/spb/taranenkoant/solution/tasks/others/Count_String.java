00bd0bab47ce504800d2347ddf1c34e9d3e3d5a0
sergun87 <nosashev@yandex.ru>
2023-04-11 00:27:19 +0300

    По файлу

---
+++ solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/others/Count_String.java
@@ -0,0 +1,18 @@
+package ru.spb.taranenkoant.solution.tasks.others;
+
+
+// Посчитать количество строк в тексте
+public class Count_String {
+
+    public static void main(String[] args) {
+        String day = "День и ночь а делаю домашку";
+        // String dayAndNight = day + " " + and + " " + night;
+        //System.out.println(day + " " + and + " " + night );
+
+        int chCount = 0;
+        for(int i=0; i<day.length(); i++){
+            chCount ++;
+            System.out.println("К-во строк: " + chCount );
+        }
+    }
+}
