695d03d8495991d817fab9bd087e0a5176078eec
Тараненко Антон Петрович <taranenko@reshenie-soft.ru>
2023-04-17 10:57:58 +0300

    [!] замечания

--- solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/reverse_string2.java
+++ solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/reverse_string2.java
@@ -8,6 +8,15 @@ public class reverse_string2 {
         String str3 = "Сегодня ветер и пыль летит в глаза";
         String[] words = new StringBuilder(str3).reverse().toString().split(" ");
         StringBuilder newStr = new StringBuilder();
+
+        /**
+         * Вот так лучше не писать, не читаемо, главное помнить мы пишем код, который могут читать другие люди
+         * добавит {} после объявления цикла и плюс ты используешь разное написание, как будто копируешь откудать
+         * старайся писать весь код сам, перебивать его руками, даже если используешь чужие наработки
+         *
+         *
+         * сам код вроде работает правильно
+         * */
         for (int i = words.length - 1; i >= 0; i--) newStr.append(words[i]).append(" ");
         System.out.println(newStr);
     }
