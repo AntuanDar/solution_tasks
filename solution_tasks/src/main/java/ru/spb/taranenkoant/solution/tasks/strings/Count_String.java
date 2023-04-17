7451489a99208c577a80f686aae82cd0eea65089
Тараненко Антон Петрович <taranenko@reshenie-soft.ru>
2023-04-17 10:57:58 +0300

    [!] замечания

--- solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/Count_String.java
+++ solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/Count_String.java
@@ -1,5 +1,12 @@
 package ru.spb.taranenkoant.solution.tasks.strings;
 
+/**
+ * Класс должен называться CountString
+ *
+ * Count_String - snake case используется только для констант, например
+ *
+ * private final static String DEFAULT_TEXT = "константы пишутся обычно final static (могут быть не static) и всегда заглавными буквами";
+ * */
 
 // Посчитать количество строк в тексте
 public class Count_String {
@@ -9,10 +16,36 @@ public class Count_String {
         // String dayAndNight = day + " " + and + " " + night;
         //System.out.println(day + " " + and + " " + night );
 
+        /**
+         * Зачем здесь переменная chCount?
+         * i - же выполняет функцию счетчика
+         * */
         int chCount = 0;
-        for(int i=0; i<day.length(); i++){
+        for(int i = 0; i < day.length(); i++) {
             chCount ++;
-            System.out.println("К-во строк: " + chCount );
+            System.out.println("К-во строк: " + chCount);
         }
+
+        /**
+         * Вроде колчество строк надо посчитать по заданию, но этот класс делает что-то не то
+         * Сначала ты берешь длину строки (общее количество символов) а потом просто делаешь столько итераций сколько символов
+         * получилось
+         * */
+
+        Count_String count_string = new Count_String();
+        int lines = count_string.countLines("Здесь наш текст " +
+                "Много строчный " +
+                "Строки которого мы и должны посчитать");
+        System.out.println(lines);
+    }
+
+
+    public int countLines(String text) {
+        /**
+         * Напиши решение здесь
+         * сначала разбей текст на отдельные строки у класса String есть такой метод найди его
+         * Дальше просто считаешь сколько получилось строк
+         * */
+        return 0;
     }
 }
--- solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/reverse_string.java
+++ solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/reverse_string.java
@@ -6,15 +6,21 @@ package ru.spb.taranenkoant.solution.tasks.strings;
 public class reverse_string {
     public static void main(String  [] args) {
 
-        String day = "День и ночь а делаю домашку";
+        String day = "    День и ночь а делаю домашку     ";
         new StringBuilder(day).reverse().toString();
         String result = new StringBuffer(day).reverse().toString();
 
+        /**
+         * что значит  //boolean String;
+         * */
         //boolean String;
         System.out.println(result);
 
     }
 
+    /**
+     * Вроде работает правильно только название исправить надо с заглавной буквы и без снейк-кейса
+     * */
 }
 
 
