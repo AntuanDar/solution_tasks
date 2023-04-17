fbe13eacd7b0366f94417f61af88fe17decf99ca
sergun87 <nosashev@yandex.ru>
2023-04-17 21:53:11 +0300

    [!] исправления

---
+++ solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/Registr.java
@@ -0,0 +1,28 @@
+package ru.spb.taranenkoant.solution.tasks.strings;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+/**
+ * Исправил 17.04.2023 разбил текст типа форма регистрации- введите фамилию имя отчество...
+ * */
+
+//Скорректировать регистр у ФИО
+public class Registr {
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите  Фамилию : ");
+        String fam = scan.nextLine();
+        System.out.print("Введите  Имя : ");
+        String name = scan.nextLine();
+        System.out.print("Введите  Отчество: ");
+        String othc = scan.nextLine();
+
+
+        System.out.print("Возвращаемое значение: " +fam.substring(0,1).toUpperCase() + fam.substring(1).toLowerCase());
+        System.out.print(" " + name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase());
+        System.out.print(" "+ othc.substring(0,1).toUpperCase() + othc.substring(1).toLowerCase());
+
+    }
+}
