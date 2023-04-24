83f80ee07c0c3fc0355c6b7eeef932e5a77e08a2
sergun87 <nosashev@yandex.ru>
2023-04-24 22:25:07 +0300

    [!] исправления к-ва строк правка 24.04.2023 Подсчет к-ва слов в введенном тексте 24.04.2023

--- solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/reverse_string2.java
+++ solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/reverse_string2.java
@@ -1,24 +1,24 @@
 package ru.spb.taranenkoant.solution.tasks.strings;
-
+import java.util.Scanner;
 
 // Развернуть слова в предложении
 //Последовательность слов не нарушена а слова развернуты
 public class reverse_string2 {
     public static void main(String[] args) {
-        String str3 = "Сегодня ветер и пыль летит в глаза";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите любой текст :");
+
+        String str3 = sc.nextLine();
+
+        //String str3 = "Сегодня ветер и пыль летит в глаза";
         String[] words = new StringBuilder(str3).reverse().toString().split(" ");
         StringBuilder newStr = new StringBuilder();
 
-        /**
-         * Вот так лучше не писать, не читаемо, главное помнить мы пишем код, который могут читать другие люди
-         * добавит {} после объявления цикла и плюс ты используешь разное написание, как будто копируешь откудать
-         * старайся писать весь код сам, перебивать его руками, даже если используешь чужие наработки
-         *
-         *
-         * сам код вроде работает правильно
-         * */
-            for (int i = words.length - 1; i >= 0; i--) newStr.append(words[i]).append(" ");
-             System.out.println(newStr);
-    }
 
+        for (int i = words.length - 1; i >= 0; i--) newStr.append(words[i]).append(" "); {
+            System.out.println(newStr);
+        }
+
+    }
 }
