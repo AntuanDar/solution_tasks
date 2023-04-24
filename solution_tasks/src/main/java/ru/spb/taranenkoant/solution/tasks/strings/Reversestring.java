a080910ab05e6521af78a9ec95234037ec525e9a
Тараненко Антон Петрович <taranenko@reshenie-soft.ru>
2023-04-24 18:22:05 +0300

    [!] замечания по ReverseString

--- solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/Reversestring.java
+++ solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/Reversestring.java
@@ -4,10 +4,19 @@ package ru.spb.taranenkoant.solution.tasks.strings;
 //Наверное надо было строку через input передать но я пока тренируюсь так
 
 // Реверс строки доработан 17.04.2023
+
+/**
+ * Ну почти, camelCase подразумевает, что каждое новое слово начинается с заглавной буквы ReverseString
+ * */
 public class Reversestring {
     public static void main(String  [] args) {
 
         String day = "    День и ночь а делаю домашку     ";
+        /**
+         * Обрати внимание идея выделяет .toString(); если навести на лампочку курсор, то будет написано, что
+         * возвращаемый результат игнорируется и это действительно так
+         * new StringBuilder(day).reverse().toString(); - строчка здесь лишняя, только отъедает процессорное время
+         * */
         new StringBuilder(day).reverse().toString();
         String result = new StringBuffer(day).reverse().toString();
 
