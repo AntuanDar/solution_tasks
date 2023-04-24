1ee979f08581a44c0d56641b8a1dc89bde14df9f
sergun87 <nosashev@yandex.ru>
2023-04-24 22:04:34 +0300

    [!] исправления к-ва строк правка 24.04.2023 Подсчет к-ва слов в введенном тексте 24.04.2023

--- solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/CountWordsSentence.java
+++ solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/CountWordsSentence.java
@@ -1,21 +1,45 @@
 package ru.spb.taranenkoant.solution.tasks.strings;
 
+import java.util.Scanner;
 import java.util.List;
 
 /**
  * Created by Anton on 22.03.2023
  * description:
  */
+
+/**
+ * Finalized by Sergey on 24.04.2023
+ * description:
+ */
+
+/**
+ * Подсчет к-ва слов в введенном тексте
+ */
 public class CountWordsSentence {
 
     public static void main(String args[]) {
 
-        String sentence = "Тут у нас рандомное предложение, любое ,      какое нравится ";
+        Scanner sc = new Scanner(System.in);
 
-        // не рабочий вариант, потому что выдает 14, а все таки слов отдельных у нас 8,
-        // а если пробелов много, то вообще может быть сколько угодно
-        String[] words = sentence.split(" ");
-        System.out.println(words.length);
+        System.out.println("Введите любой текст :");
+
+        String input = sc.nextLine();
+
+        int count = 0;
+
+        if (input.length() != 0) {
+            count++;
+
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == ' ') {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("Вы ввели " + count + " слов");
+
 
     }
 }
