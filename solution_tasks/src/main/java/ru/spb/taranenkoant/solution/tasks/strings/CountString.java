8aaa5c0f977bafedfc56fc84018ad7af3028543a
sergun87 <nosashev@yandex.ru>
2023-05-03 21:01:01 +0300

    Перенос строки 3 мая

--- solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/CountString.java
+++ solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/CountString.java
@@ -56,7 +56,13 @@ public class CountString {
          * надо учесть оба варианта переноса строк
          * */
 
-        String[] words = str.split("\\s"); //splits the string based on whitespace
+
+        /**
+         *
+        03.05.2023  Добавлено \r\n для переноса строки
+         * */
+
+        String[] words = str.split("\r\n"); //splits the string based on whitespace
         System.out.println("Количество строк " + (words.length));
     }
 }
