8d6dfaa9291f9e002509dbb3c805786c6baa9c1f
Anton <antuan_dar@bk.ru>
2023-05-12 00:13:19 +0300

    [!] примечание к Randomstring

--- solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/Randomstring.java
+++ solution_tasks/src/main/java/ru/spb/taranenkoant/solution/tasks/strings/Randomstring.java
@@ -1,9 +1,26 @@
 package ru.spb.taranenkoant.solution.tasks.strings;
 
+/**
+ * C классом что-то не так, возможно неудачный мердж
+ *
+ * по классу самому нейминг Randomstring -> RandomString
+ *
+ *
+ * String str = "ddgdgdgehdhsh45fhfhdhs"; - почему именно такой набор символов?
+ *  Выравнивание StringBuffer sb=new StringBuffer(); -> StringBuffer sb = new StringBuffer();
+ *
+ *  for(int i =0, i<lenght ++){ -> for(int i = 0, i < lenght ++) {
+ *
+ *  for(int i =0, i<lenght ++){ - с этой строкой совсем все плохо
+ *  откуда взялась переменная lenght, после нее так же нет ; ++ - непонятно к чему относится
+ *
+ *  выравнивание можно поправить по ctrl + alt + L
+ */
 
+import java.util.Random;
 
 // Generation to String 11.05.2023
-public class Randomstring String getRandomString(int lenght){
+public class Randomstring { String getRandomString(int lenght){
     String str = "ddgdgdgehdhsh45fhfhdhs";
     Random random = new Random();
     StringBuffer sb=new StringBuffer();
